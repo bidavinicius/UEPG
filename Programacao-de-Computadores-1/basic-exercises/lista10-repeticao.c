2d62ea2c0e3c748a0bb2b2a0375b6c2ef226c065
bidavinicius <bidaviniciusribas@gmail.com>
2026-02-27 01:55:55 -0300

    refactor: moved file

---
+++ Programacao-de-Computadores-1/basic-exercises/lista10-repeticao.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Lista de Exercicios 10 - Repetição
+Autor: Vinicius Ribas Bida
+RA: 25070323
+Data: 19/05/2025
+*/
+
+int main()
+{
+/*
+Exercicio 1
+int n;
+
+printf("Digite um numero inteiro positivo: ");
+scanf("%d", &n);
+
+printf("Numeros naturais de 0 até %d:\n", n);
+for (int i = 0; i <= n; i++) {
+    printf("%d\n", i);
+}
+
+
+
+Exercicio 2
+int n;
+
+printf("Digite um numero inteiro positivo: ");
+scanf("%d", &n);
+
+printf("Numeros naturais de %d até 0:\n", n);
+for (int i = n; i >= 0; i--) {
+    printf("%d\n", i);
+}
+
+
+
+Exercicio 3
+int n;
+
+printf("Digite um numero inteiro: ");
+scanf("%d", &n);
+
+printf("Os %d primeiros numeros impares sao:\n", n);
+for (int i = 0; i < n; i++) {
+    printf("%d\n", 2*i+1);
+}
+
+
+
+Exercicio 4
+printf("Os cinco primeiros numeros multiplos de 3 sao:\n");
+for (int i = 1; i<=5; i++){
+    printf("%d\n", 3*i);
+}
+
+
+
+Exercicio 5
+int n, soma=0;
+
+for (int i = 1; i<=50; i++){
+    n=2*i;
+    soma=soma+n;
+}
+printf("A soma dos 50 primeiros numeros pares e: %d", soma);
+
+
+
+Exercicio 6
+for (int i = 10; i>=0; i--){
+    printf("%d\n", i);
+}
+printf("FIM");
+
+
+
+Exercicio 7
+int n, linha = 1;
+
+printf("Digite um numero inteiro positivo:");
+scanf("%d", &n);
+
+for (int i = 1; i<=n; i++){
+    for (int j = 1; j<=i; j++){
+        printf("%d  ", linha);
+        linha++;
+    }
+    printf("\n");
+}
+
+
+
+Exercicio 8
+int n, a=0;
+
+printf("Digite um numero maior que 1: ");
+scanf("%d", &n);
+
+for(int i = 2; i<=n/2; i++){
+    if(n % i == 0){
+        a = 1;
+        break;
+    }
+}if(a!=1){
+    printf("O numero %d e primo", n);
+}else printf("O numero %d nao e primo", n);
+*/
+
+return 0;
+}
